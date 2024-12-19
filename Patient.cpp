ce1761a9909bb5415fc24176371b03fde5d0f037
HaimA16 <haimarmiass@gmail.com>
2024-12-19 12:45:30 +0200

    added

--- Patient.cpp
+++ Patient.cpp
@@ -1,5 +1,73 @@
 //
-// Created by ilanz on 19/12/2024.
+// Created by ilanz on 09/12/2024.
 //
-
+#include "iostream"
+#include "string"
 #include "Patient.h"
+using namespace std;
+
+Patient::Patient(string Name, long Id, string Phone_num, long Password, string Healt_Provider): User(Name,Id,Phone_num,Password){
+    this->Health_Provider=Healt_Provider;
+}
+char *Patient::get_type() const {
+    return "Patient";
+}
+
+void Patient::saveToFile(const string &filename) const{
+    ofstream file(filename,ios::app);
+    if (file.is_open()){
+        file<<Name<<endl;
+        file<<id<<endl;
+        file<<Phone_Number<<endl;
+        file<<Password<<endl;
+        file<<Health_Provider<<endl;
+        file << "-------------------" << endl;
+        file.close();
+    }
+    else{
+        cout<<"Error: Could not open file for writing." << endl;
+    }
+}
+
+vector<Patient> Patient::readFromFile(const string &filename) {
+    ifstream file(filename);
+    vector<Patient> patients;
+    if (file.is_open()){
+        string name, phone, healthProvider;
+        long id, password;
+        while (getline(file, name)) {
+            if (name == "-------------------") {
+                continue;
+            }
+            file >> id;
+            file.ignore();
+            getline(file, phone);
+            file >> password;
+            file.ignore();
+            getline(file, healthProvider);
+            patients.emplace_back(name, id, phone, password, healthProvider);
+        }
+        file.close();
+    }
+    else {
+        cout << "Error: Could not open file for reading." << endl;
+    }
+    return patients;
+}
+
+string Patient::get_health_provider() const {
+    return Health_Provider;
+}
+
+void Patient::set_phone(string phone_num) {
+    this->Phone_Number=phone_num;
+}
+
+void Patient::set_healt_care(string health_care) {
+    this->Health_Provider=health_care;
+}
+
+
+
+
+
--- Patient.h
+++ Patient.h
@@ -1,14 +1,26 @@
 //
-// Created by ilanz on 19/12/2024.
+// Created by ilanz on 09/12/2024.
 //
 
-#ifndef PROJECT_YESODOT_PATIENT_H
-#define PROJECT_YESODOT_PATIENT_H
+#ifndef PROJECT_PATIENT_H
+#define PROJECT_PATIENT_H
+#include "User.h"
+#include "string"
+using namespace std;
 
-
-class Patient {
+class Patient: public User{
+private:
+    string Health_Provider;
+public:
+    Patient(string Name,long Id,string Phone_num,long Password,string Healt_Provider);
+    char *get_type()const;
+    void saveToFile(const string &filename) const;
+    static vector<Patient> readFromFile(const string &filename);
+    string get_health_provider()const;
+    void set_phone(string phone_num);
+    void set_healt_care(string health_care);
 
 };
 
 
-#endif //PROJECT_YESODOT_PATIENT_H
+#endif //PROJECT_PATIENT_H
