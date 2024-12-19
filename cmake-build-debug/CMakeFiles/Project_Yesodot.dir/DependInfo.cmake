945c8700470ecc8e3968eb3caa6411054ace79c5
Ilan Merkovich <merkovichilan@gmail.com>
2024-12-19 13:12:37 +0200

    final release

---
+++ cmake-build-debug/CMakeFiles/Project_Yesodot.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/ilanz/CLionProjects/Project_Yesodot/Appointment.cpp" "CMakeFiles/Project_Yesodot.dir/Appointment.cpp.obj" "gcc" "CMakeFiles/Project_Yesodot.dir/Appointment.cpp.obj.d"
+  "C:/Users/ilanz/CLionProjects/Project_Yesodot/Doctor.cpp" "CMakeFiles/Project_Yesodot.dir/Doctor.cpp.obj" "gcc" "CMakeFiles/Project_Yesodot.dir/Doctor.cpp.obj.d"
+  "C:/Users/ilanz/CLionProjects/Project_Yesodot/Patient.cpp" "CMakeFiles/Project_Yesodot.dir/Patient.cpp.obj" "gcc" "CMakeFiles/Project_Yesodot.dir/Patient.cpp.obj.d"
+  "C:/Users/ilanz/CLionProjects/Project_Yesodot/User.cpp" "CMakeFiles/Project_Yesodot.dir/User.cpp.obj" "gcc" "CMakeFiles/Project_Yesodot.dir/User.cpp.obj.d"
+  "C:/Users/ilanz/CLionProjects/Project_Yesodot/main.cpp" "CMakeFiles/Project_Yesodot.dir/main.cpp.obj" "gcc" "CMakeFiles/Project_Yesodot.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Project_Yesodot.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_Yesodot.dir/Appointment.cpp.obj"
+  "CMakeFiles/Project_Yesodot.dir/Appointment.cpp.obj.d"
+  "CMakeFiles/Project_Yesodot.dir/Doctor.cpp.obj"
+  "CMakeFiles/Project_Yesodot.dir/Doctor.cpp.obj.d"
+  "CMakeFiles/Project_Yesodot.dir/Patient.cpp.obj"
+  "CMakeFiles/Project_Yesodot.dir/Patient.cpp.obj.d"
+  "CMakeFiles/Project_Yesodot.dir/User.cpp.obj"
+  "CMakeFiles/Project_Yesodot.dir/User.cpp.obj.d"
+  "CMakeFiles/Project_Yesodot.dir/main.cpp.obj"
+  "CMakeFiles/Project_Yesodot.dir/main.cpp.obj.d"
+  "Project_Yesodot.exe"
+  "Project_Yesodot.exe.manifest"
+  "Project_Yesodot.pdb"
+  "libProject_Yesodot.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_Yesodot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
