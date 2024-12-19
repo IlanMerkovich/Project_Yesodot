a2c87e483ba2c4ffefe37e2d7d0d35e054f25a38
orel levy <oreallevy@gmail.com>
2024-12-19 12:36:36 +0200

    added user

--- User.cpp
+++ User.cpp
@@ -1,5 +1,30 @@
 //
-// Created by ilanz on 19/12/2024.
+// Created by ilanz on 21/11/2024.
 //
 
 #include "User.h"
+#include "string"
+using namespace std;
+
+User::User(string Name, long Id, string Phone_num,long Password){
+    this->Name=Name;
+    this->id=Id;
+    this->Phone_Number=Phone_num;
+    this->Password=Password;
+}
+
+long long User::get_id() const {
+    return id;
+}
+
+string User::get_name() const {
+    return Name;
+}
+
+long User::get_password() const {
+    return Password;
+}
+
+string User::get_phone() const {
+    return Phone_Number;
+}
--- User.h
+++ User.h
@@ -1,14 +1,27 @@
 //
-// Created by ilanz on 19/12/2024.
+// Created by ilanz on 21/11/2024.
 //
 
-#ifndef PROJECT_YESODOT_USER_H
-#define PROJECT_YESODOT_USER_H
-
-
-class User {
-
+#ifndef PROJECT_USER_H
+#define PROJECT_USER_H
+#include "string"
+#include "vector"
+#include "Appointment.h"
+using namespace std;
+class User{
+protected:
+    string Name;
+    long long id;
+    long Password;
+    string Phone_Number;
+public:
+    User(string Name,long Id,string Phone_num,long Password);
+    virtual char *get_type()const=0;
+    string get_name()const;
+    long long get_id()const;
+    long get_password()const;
+    string get_phone()const;
 };
 
 
-#endif //PROJECT_YESODOT_USER_H
+#endif //PROJECT_USER_H
